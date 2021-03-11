0138e06c7877c78d5e79062dd88deb185f43c118
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2021-03-11 14:25:21 +0200

    Add command for s3 notification trigger

--- scripts/create-bucket.sh
+++ scripts/create-bucket.sh
@@ -7,3 +7,4 @@ aws --endpoint-url=$AWS_ENDPOINT s3api create-bucket --bucket data-transform-loc
 aws --endpoint-url=$AWS_ENDPOINT s3api create-bucket --bucket xml-data --region eu-central-1
 aws --endpoint-url=$AWS_ENDPOINT s3api create-bucket --bucket json-data --region eu-central-1
 aws --endpoint-url=$AWS_ENDPOINT s3 cp src/data s3://xml-data --recursive
+aws --endpoint-url=$AWS_ENDPOINT s3api put-bucket-notification-configuration --bucket xml-data --notification-configuration '{"LambdaFunctionConfigurations":[{"Events":["s3:ObjectCreated:*"],"LambdaFunctionArn":"arn:aws:lambda:eu-central-1:000000000000:function:data-transform-local-processing"}]}'
